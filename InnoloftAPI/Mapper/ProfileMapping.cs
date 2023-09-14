6bf20acd31a748d41d4cb984f63fba463395f6b0
softdeveloper968 <113977118+softdeveloper968@users.noreply.github.com>
2023-09-14 11:44:39 +0530

    mapper

---
+++ InnoloftAPI/Mapper/ProfileMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using InnoloftAPI.Core.Model;
+using InnoloftAPI.Core.Resources;
+
+namespace InnoloftAPI.Mapper
+{
+    public class ProfileMapping : Profile
+    {
+        public ProfileMapping()
+        {
+            CreateMap<EventViewModel, Event>().ReverseMap();
+            CreateMap<ParticipantsViewModel, Participant>().ReverseMap();
+        }
+    }
+}
